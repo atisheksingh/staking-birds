33fd61138e9111ca834d798f607ec373f6f88cff
RohitPandit <panditrohit532@gmail.com>
2023-07-30 13:26:38 +0530

    Updated the about section add demo content & show image right side.

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,6 +19,19 @@ const useStyles = makeStyles((theme) => ({
     height: "43px",
     lineHeight: "29px",
   },
+  aboutTitle: {
+    display: "block",
+    fontSize: "1.5em",
+    marginBlockStart: "0.83em",
+    marginBlockEnd: "0.83em",
+    marginInlineStart: 0,
+    marginInlineEnd: 0,
+    fontWeight: "bold",
+  },
+  img: {
+    float: "right",
+    height: "10rem"
+  }
 }));
 const About = () => {
   const classes = useStyles();
@@ -26,6 +39,15 @@ const About = () => {
     <>
       <div className={classes.dynamicheader}>
         <div className={classes.dynamicTitle}>About</div>
+      </div>
+      <h2 className={classes.aboutTitle}>About</h2>
+      <div>
+        <img className={classes.img} src="/images/AboutBirds.png" />
+        Why do people eat swiftlet nests?
+        The appeal of edible bird's nest
+        Swiftlet nests' immune-boosting properties were once a product of Chinese myth. In recent years, swiftlet farmers and food scientists have been investing in research to prove the nests' nutritional benefits.
+        <br /><br /><br />
+        Nutritional studies have shown the saliva to be mainly made up of protein, followed by carbohydrates. In Shanghai restaurants sell the “the caviar of the East,” as it is known, for hundreds of dollars a bowl.11
       </div>
     </>
   );
