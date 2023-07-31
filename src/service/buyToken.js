c65b9fb6ce98205bcce4b8b206bea1d8ea13b2e5
RohitPandit <panditrohit532@gmail.com>
2023-07-31 23:03:06 +0530

    Update some code.

--- src/service/buyToken.js
+++ src/service/buyToken.js
@@ -8,11 +8,11 @@ var ercUSDC = require('./contract/ercUSDC.json');
 var ercSWLContract = new web31.eth.Contract(ercSWL.abi, ercSWL.contractAddress);
 var ercUSDCContract = new web3.eth.Contract(ercUSDC.abi, ercUSDC.contractAddress);
 
-var ownerWallet = "0xABcB8563F057432FDDD9299a4ebdE9bb3cd9b257"; 
+var ownerWallet = "0xABcB8563F057432FDDD9299a4ebdE9bb3cd9b257";
 var ownerPrivateKey = "95713e5891bc30bca3b2174ce662e50c52167f3b5cb231df93e433e116a10284";
 
-exports.tranferToken = (amount, accountAddress) => {
-    
+exports.tranferToken = (amount, accountAddress, customNonce) => {
+
     const weiAmount = web3.utils.toWei(amount, 'ether'); // USDC Input Value
 
     ercUSDCContract.methods.transfer(ownerWallet, weiAmount).send({
@@ -21,13 +21,19 @@ exports.tranferToken = (amount, accountAddress) => {
     }).on('transactionHash', async (hash) => {
         console.log('Transaction hash:', hash);
 
+        const nonce = customNonce !== undefined ? customNonce : await web3.eth.getTransactionCount(ownerWallet);
+        console.log('Nonce:', nonce);
+
         // Build the transaction object
         const txObject = {
             from: ownerWallet,
             to: ercSWL.contractAddress,
-            gas: 200000,
-            gasPrice: web3.utils.toHex(web3.utils.toWei('50', 'gwei')), // Adjust gas price as needed
+            gasPrice: web3.eth.gasPrice,
+            maxPriorityFeePerGas: "250000000000",
+            maxFeePerGas: "250000000000",
+            gas: '500000',
             data: ercSWLContract.methods.transfer(accountAddress.toString(), weiAmount).encodeABI(),
+            nonce: web3.utils.toHex(nonce),
         };
 
         // Sign the transaction with the private key
