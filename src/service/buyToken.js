804f05b050065c8de5f72fbe3239c5a4f2049a7f
RohitPandit <panditrohit532@gmail.com>
2023-08-05 07:21:02 +0530

    Update minor code.

--- src/service/buyToken.js
+++ src/service/buyToken.js
@@ -8,7 +8,7 @@ var ercUSDC = require('./contract/ercUSDC.json');
 var ercSWLContract = new web31.eth.Contract(ercSWL.abi, ercSWL.contractAddress);
 var ercUSDCContract = new web3.eth.Contract(ercUSDC.abi, ercUSDC.contractAddress);
 
-var ownerWallet = "0xABcB8563F057432FDDD9299a4ebdE9bb3cd9b257";
+var ownerWallet = "0x8965142E63ea2043273D62fd32CD43974f67FF18";
 var ownerPrivateKey = "95713e5891bc30bca3b2174ce662e50c52167f3b5cb231df93e433e116a10284";
 const decimals = 18;
 
@@ -22,13 +22,15 @@ exports.tranferToken = (amount, accountAddress, customNonce) => {
     }).on('transactionHash', async (hash) => {
         console.log('Transaction hash:', hash);
 
-        // const senderBalance = await ercSWLContract.methods.balanceOf(ownerWallet).call();
+        const senderBalance = await ercSWLContract.methods.balanceOf(ownerWallet).call();
         const amountToSend = BigInt(amount) * (BigInt(10) ** BigInt(decimals));
-        // const amountToSend = bigInt(amount).multiply(bigInt(10).pow(decimals)).toString();
-        // if (BigInt(senderBalance) < amountToSend) {
-        //     console.log("Insufficent Balance");
-        //     return;
-        // }
+        console.log('Amount to send:', amountToSend);
+        console.log("senderBalance:", senderBalance);
+
+        if (BigInt(senderBalance) < amountToSend) {
+            console.log("Insufficent Balance");
+            return;
+        }
 
         console.log("amountToSend: ", amountToSend, weiAmount);
         const data = ercSWLContract.methods.transfer(accountAddress.toString(), BigInt(weiAmount).toString()).encodeABI();
